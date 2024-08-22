6a63e544245ddf89ad2bbe4309877dc692053e90
_rogala <jk.rogala@student.uw.edu.pl>
2024-08-22 11:19:35 +0200

    Add project files.

---
+++ TDMController/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TDMController.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
