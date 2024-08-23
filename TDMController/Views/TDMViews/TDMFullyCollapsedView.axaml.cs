2e31c8f726fbadfaf80bac08feb8255fdd772e90
_rogala <jk.rogala@student.uw.edu.pl>
2024-08-23 16:40:14 +0200

    Add Responsive Layouts

---
+++ TDMController/Views/TDMViews/TDMFullyCollapsedView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TDMController.Views.TDMViews;
+
+public partial class TDMFullyCollapsedView : UserControl
+{
+    public TDMFullyCollapsedView()
+    {
+        InitializeComponent();
+    }
+}
