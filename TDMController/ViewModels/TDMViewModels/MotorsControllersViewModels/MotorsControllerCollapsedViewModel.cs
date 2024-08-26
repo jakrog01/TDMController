5e4a153560139b0fb103e9c0a018cfd91473d51a
_rogala <jk.rogala@student.uw.edu.pl>
2024-08-26 15:05:52 +0200

    Add Collapsed Motor Controller Widget Add TextBlock and TextBox styles

---
+++ TDMController/ViewModels/TDMViewModels/MotorsControllersViewModels/MotorsControllerCollapsedViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Material.Icons;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDMController.ViewModels.TDMViewModels
+{
+    public partial class MotorsControllerCollapsedViewModel : ViewModelBase
+    {
+        [ObservableProperty]
+        private String? _branchLabel = "BRANCH X";
+
+        public ObservableCollection<MotorControllerItem> MotorControllers{ get; } = new()
+        {
+            new MotorControllerItem("ROTATION", MaterialIconKind.ChevronRight, MaterialIconKind.ChevronLeft),
+            new MotorControllerItem("POSITION", MaterialIconKind.ChevronRight, MaterialIconKind.ChevronLeft)
+        };
+    }
+
+    public class MotorControllerItem (string label, MaterialIconKind iconForward, MaterialIconKind iconBackward)
+    {
+        public string Label { get; } = label;
+        public MaterialIconKind IconForward { get; } = iconForward;
+
+        public MaterialIconKind IconBackward { get; } = iconBackward;
+    }
+}
